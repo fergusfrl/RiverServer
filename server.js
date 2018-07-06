dd3b9085690c1424a8aba18c78f9e08e0d58c06e
LAPTOP\Fergus <fergusfrl@gmail.com>
2018-07-07 10:29:24 +1200

    added MLab connection

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const express = require("express");
-const app = express();
+const express = require("express"),
+    mongoose = require("mongoose"),
+    app = express(),
+    db = require("./config/keys").mongoURI;
+
+// Connect to MLab MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
 
 const port = process.env.PORT || 3031;
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
