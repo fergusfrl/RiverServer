5f12d4067a8aa2ac0d71ea2d07f990373951c312
Fergus Farrell <fergusfrl@gmail.com>
2018-10-30 08:39:25 +1300

    Added swimming guide schema

---
+++ src/models/guides/SwimmingGuide.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const SwimmingGuideSchema = new Schema({
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    river: { type: String },
+    region: { type: String },
+    activity: {
+      type: String,
+      required: true
+    },
+    gaugeName: { type: String },
+    markers: [],
+    lat: { type: Number },
+    lng: { type: Number },
+    description: {
+        type: String,
+        required: true
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = SwimmingGuide = mongoose.model("swimmingGuides", SwimmingGuideSchema);
